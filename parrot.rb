0bdecb3a0bece776712f147813c54ab9e7dbffbc
proud-entropy-9548 <e.e.seieroe@gmail.com>
2018-11-09 23:50:09 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot()
-  puts "string"
-  returns "string"
+def parrot(argument)
+  puts argument
+  return argument
   
 end
